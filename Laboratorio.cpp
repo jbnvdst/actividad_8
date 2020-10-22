8d7a073b4df8289ee37723977135295b8f6c8fb2
jbnvdst <70921454+jbnvdst@users.noreply.github.com>
2020-10-21 19:52:25 -0500

    Terminado actividad 9

--- Laboratorio.cpp
+++ Laboratorio.cpp
@@ -1,4 +1,5 @@
 #include "Laboratorio.h"
+#include <fstream>
 
 Laboratorio::Laboratorio()
 {
@@ -35,4 +36,57 @@ void Laboratorio::mostrar()
         //cout << "Memoria Interna: " << z.getMemoria(); << endl;
         //cout << "Marca: " << z.getMarca(); << endl;
     }
+}
+
+
+void Laboratorio::respaldar_tabla()
+{
+    ofstream archivo("computadoras.txt");
+    if(archivo.is_open())
+    {
+        archivo << left;
+        archivo << setw(10) << "Sistema Operativo";
+        archivo << setw(6) << "Memoria Ram";
+        archivo << setw(6) << "Memoria Interna";
+        archivo << setw(10) <<"Marca";
+        arch << endl;
+        for(size_t i = 0;  i < cont; i++)
+    {
+            computadora &z = arreglo[i];
+            archivo << z;
+    }
+
+    archivo.close();
+    }
+}
+
+void Laboratorio::recuperar()
+{
+    ifstream archivo("computadoras.txt");
+
+    if(archivo.is_open())
+    {
+        string temp;
+        computadora z;
+
+        while (true)
+        {
+            getline(archivo, temp);
+            z.setSistema(temp);
+
+            getline(archivo, temp);
+            z.setRam(temp);
+
+            getline(archivo, temp);
+            z.setMemoria(temp);
+
+            getline(archivo, temp);
+            z.setMarca(temp);
+
+            agregarComputadora(z)
+
+        }
+    }
+
+    archivo.close();
 }
